cd86ff4a5a16337f2dc9ec05ee8d8dc9ca88bc23
stellarbears <stellarbears@gmail.com>
2018-08-24 11:40:08 +0300

    Public modifier added

--- MaterialSharp/MaterialSharp.cs
+++ MaterialSharp/MaterialSharp.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
 
 namespace MaterialSharp
 {
-    class MaterialSharp
+    public class MaterialSharp
     {
         public static Version Version => Assembly.GetExecutingAssembly().GetName().Version;
     }
