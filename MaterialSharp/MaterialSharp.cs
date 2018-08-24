0bc6f61213a25c8485feee34418775fee71d0cdc
stellarbears <stellarbears@gmail.com>
2018-08-24 11:28:56 +0300

    0.2. Verison support added

---
+++ MaterialSharp/MaterialSharp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace MaterialSharp
+{
+    class MaterialSharp
+    {
+        public static Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+    }
+}
